bad11f4b388273ce05db9a4ed28610c723373138
nabin11bh <nabinbhattarai214@gmail.com>
2026-01-12 16:12:24 +0545

    feat(admin):add dashboard controller

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,29 @@
- import express from "express"
- import cors from "cors"
- import authRoutes from "./route/auth.route";
- import userRoutes from "./route/user.routes";
- import serviceRoutes from "./route/service.route"
- import bookingRoute from "./route/booking.route";
+import express from "express"
+import cors from "cors"
+import authRoutes from "./route/auth.route";
+import userRoutes from "./route/user.routes";
+import adminRoutes from "./route/admin.route";
+import serviceRoutes from "./route/service.route"
+import bookingRoute from "./route/booking.route";
 import { getMyProfile, updateMyProfile } from "./controller/users/user.controller";
 
- 
- const app = express()
- app.use(cors());
- app.use(express.json())
- app.use("/api/auth",authRoutes)
- app.use("/api/users",userRoutes)
- app.use("/api/services", serviceRoutes);
- app.use("/api/bookings", bookingRoute);
- app.use("/api/bookings", bookingRoute);
- app.use("/api/profile",getMyProfile)
- app.use("/api/profile",updateMyProfile)
 
- 
- 
+const app = express()
+app.use(cors());
+app.use(express.json())
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/admin", adminRoutes)
+app.use("/api/services", serviceRoutes);
+app.use("/api/bookings", bookingRoute);
+app.use("/api/profile", getMyProfile)
+app.use("/api/profile", updateMyProfile)
 
 
- 
 
 
- export default app
+
+
+
+
+export default app
