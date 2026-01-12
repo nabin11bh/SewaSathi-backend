ab932da83864cc4bc858472bf5f2ad24fb48e171
nabin11bh <nabinbhattarai214@gmail.com>
2026-01-12 16:29:27 +0545

    route(admin):dashboard and bookings

---
+++ src/route/admin.route.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { authenticate } from "../middleware/auth.middleware";
+import { authorize } from "../middleware/role.middleware";
+import { getDashboardStats, getAllBookings } from "../controller/admin/admin.controller";
+
+const router = Router();
+
+// admin dashboard stats
+router.get(
+  "/stats",
+  authenticate,          
+  authorize("admin"),    
+  getDashboardStats      
+);
+
+
+// Admin All Bookings
+router.get(
+  "/bookings",
+  authenticate,          
+  authorize("admin"),    
+  getAllBookings         
+);
+
+export default router;
